3dd95d29b7ebc18d2d80d08b651cec3613104d34
Invntrm <me@invntrm.ru>
2014-07-28 22:03:17 +0400

    Improve mailProject()

--- common.php
+++ common.php
@@ -630,7 +630,8 @@ function mailDump($data, $to, $consts, $theme)
  * @return bool
  */
 function mailProject($message, $to, $fromName, $consts, $theme) {
-    $fromName = $fromName ? "$fromName (via site)" : $consts['PROJECT_NAME'];
+    $fromName = transliterateCyr($fromName);
+    $fromName = $fromName ? "$fromName (via site)" : $consts['PROJECT_NAME_STUB'];
     $from = "$fromName <{$consts['MAILER_EMAIL']}>";
     //
     // MIME message type
@@ -734,6 +735,19 @@ function recursiveDegenerateArrOptimize($arr)
     }
     return $arr;
 }
+
+
+/**
+ * @link https://gist.github.com/vindia/1476814
+ * @param $string
+ * @return mixed
+ */
+function transliterateCyr($string) {
+    $roman = array("Sch","sch",'Yo','Zh','Kh','Ts','Ch','Sh','Yu','ya','yo','zh','kh','ts','ch','sh','yu','ya','A','B','V','G','D','E','Z','I','Y','K','L','M','N','O','P','R','S','T','U','F','','Y','','E','a','b','v','g','d','e','z','i','y','k','l','m','n','o','p','r','s','t','u','f','','y','','e');
+    $cyrillic = array("Щ","щ",'Ё','Ж','Х','Ц','Ч','Ш','Ю','я','ё','ж','х','ц','ч','ш','ю','я','А','Б','В','Г','Д','Е','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Ь','Ы','Ъ','Э','а','б','в','г','д','е','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','ь','ы','ъ','э');
+    return str_replace($cyrillic, $roman, $string);
+}
+
 
 /**
  * @example $code = encrypt_data('12345','Привет Ясень! 45');
